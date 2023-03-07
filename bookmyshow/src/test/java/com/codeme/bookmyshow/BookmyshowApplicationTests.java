4d288e2e13534861b1277120b905c444a5f77047
rawats <rawats@opentext.com>
2023-03-07 17:57:49 +0530

    Initial structure

---
+++ bookmyshow/src/test/java/com/codeme/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeme.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
